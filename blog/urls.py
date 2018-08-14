0660b669b7c4cf2a4c359531b2141924421f2aaf
elcior.carvalho <elcior.carvalho@comlink.com.br>
2018-08-14 13:41:59 -0300

    Upload do virtual env

---
+++ blog/urls.py
@@ -0,0 +1,7 @@
+
+from django.urls import path
+from . import views
+
+utlpatterns = [
+    path('', views.post_list, name='post_list')
+]
---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\elcior.carvalho\documents\__elcior_design\py-dev\djangogirls\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
